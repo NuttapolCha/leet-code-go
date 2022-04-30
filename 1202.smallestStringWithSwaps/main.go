3a5629c6b90e1e9f93141fc678333e475fe39f06
nattapol <nuttapol.c@blockfint.com>
2022-04-30 15:41:08 +0700

    🚧 construct linked components is WIP

--- 1202.smallestStringWithSwaps/main.go
+++ 1202.smallestStringWithSwaps/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
@@ -22,116 +23,63 @@ func main() {
 	fmt.Println(smallestStringWithSwaps("vbjjxgdfnru", pairs))
 }
 
-type minStringHeap struct {
-	a   []string
-	set map[string]bool
-}
-
-func NewMinStringHeap() minStringHeap {
-	return minStringHeap{
-		a:   make([]string, 0),
-		set: make(map[string]bool),
-	}
-}
-
-func (h *minStringHeap) Display() {
-	for i, s := range h.a {
-		fmt.Printf("index: %d => '%s'\n", i, s)
-	}
-}
-
-func (h *minStringHeap) EnQueue(s string) {
-	h.a = append(h.a, s)
-	h.set[s] = true
-	h.fixUp(len(h.a) - 1)
-}
-
-func (h *minStringHeap) DeQueue() string {
-	out := h.Peek()
-
-	// move the right most node to the first
-	delete(h.set, out)
-	h.a[0] = h.a[len(h.a)-1]
-	h.a = h.a[:len(h.a)-1]
-	h.fixDown(0)
-
-	return out
-}
-
-func (h *minStringHeap) IsEmpty() bool {
-	return len(h.a) == 0
-}
-
-func (h *minStringHeap) Peek() string {
-	if h.IsEmpty() {
-		panic("queue is empty!")
-	}
-
-	return h.a[0]
-}
-
-func (h *minStringHeap) fixUp(k int) {
-	parentIndex := (k - 1) / 2
-	if parentIndex < 0 {
-		return
-	}
-
-	if h.a[k] < h.a[parentIndex] {
-		h.a[k], h.a[parentIndex] = h.a[parentIndex], h.a[k]
-		h.fixUp(parentIndex)
-	}
-}
-
-func (h *minStringHeap) fixDown(k int) {
-	leftChildIndex := 2*k + 1
-	rightChildIndex := 2*k + 2
-
-	if leftChildIndex < len(h.a) && h.a[leftChildIndex] < h.a[k] {
-		h.a[leftChildIndex], h.a[k] = h.a[k], h.a[leftChildIndex]
-		h.fixDown(leftChildIndex)
-	}
-
-	if rightChildIndex < len(h.a) && h.a[rightChildIndex] < h.a[k] {
-		h.a[rightChildIndex], h.a[k] = h.a[k], h.a[rightChildIndex]
-		h.fixDown(rightChildIndex)
-	}
-}
-
 func smallestStringWithSwaps(s string, pairs [][]int) string {
-	h := NewMinStringHeap()
-	set := make(map[string]bool) // will stop swapping if swap result already exist in set
+	linkedIndicesList := getLinkedIndices(pairs)
 
-	// initial from input
-	h.EnQueue(s)
-	set[s] = true
-
-	doSwap(&h, s, pairs)
-
-	// h.Display()
-	return h.Peek()
-}
-
-func doSwap(h *minStringHeap, s string, pairs [][]int) {
-	// fmt.Printf("original s = %s\n", s)
-
-	for _, pair := range pairs {
-		index1 := pair[0]
-		index2 := pair[1]
-		// ensure that index1 always less than index2
-		if index2 < index1 {
-			index1, index2 = index2, index1
-		}
-
-		// swap chars
-		if index1 != index2 {
-			s = s[0:index1] + string(s[index2]) + s[index1+1:index2] + string(s[index1]) + s[index2+1:]
-		}
-
-		// fmt.Printf("after swap index (%d,%d) s = %s\n", index1, index2, s)
-
-		if !h.set[s] { // not exist in set
-			h.EnQueue(s)
-			doSwap(h, s, pairs)
+	smallestStr := s
+	for _, linkedIndices := range linkedIndicesList {
+		buff := getCharBuffersFromSelectedIndices(s, linkedIndices)
+		swapedStr := sortStringOnlySelectedIndices(smallestStr, buff)
+		if swapedStr < smallestStr {
+			smallestStr = swapedStr
 		}
 	}
+
+	return smallestStr
+}
+
+func getLinkedIndices(pairs [][]int) [][]int {
+	// ret := make([][]int, 0)
+
+	// linkedElems := make(map[int][]int, 0)
+	// for _, pair := range pairs {
+	// }
+	return nil
+}
+
+type charBuffer struct {
+	char          byte
+	originalIndex int
+	newIndex      int
+}
+
+func getCharBuffersFromSelectedIndices(s string, indices []int) []charBuffer {
+	buff := make([]charBuffer, 0)
+
+	for _, index := range indices {
+		buff = append(buff, charBuffer{
+			char:          s[index],
+			originalIndex: index,
+		})
+	}
+
+	return buff
+}
+
+func sortStringOnlySelectedIndices(s string, buff []charBuffer) string {
+	chars := []byte(s)
+	sort.Slice(buff, func(i, j int) bool {
+		if buff[i].char < buff[j].char {
+			buff[i].newIndex = j
+			buff[j].newIndex = i
+			return true
+		}
+		return false
+	})
+
+	for _, each := range buff {
+		chars[each.originalIndex] = chars[each.newIndex]
+	}
+
+	return string(chars)
 }
