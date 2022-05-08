cff64cef08693f048b2bdefe31a71e84402c02fd
nattapol <nuttapol.c@blockfint.com>
2022-05-08 20:49:54 +0700

    🚧 try bsearch but not work

--- 456.132pattern/main.go
+++ 456.132pattern/main.go
@@ -1,75 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	testCases := [][]int{
-		// {1, 0, 1, 1, 1, 1, 1, 3, 2, 1, 0, -1}, // true
-		// {1, 2, 3, 4},                          // false
-		// {3, 1, 4, 2},                          // true
-		// {3, 5, 0, 3, 4},                       // true
-		{-2, 1, 2, -2, 1, 2}, // true
+		{1, 0, 1, 1, 1, 1, 1, 3, 2, 1, 0, -1}, // true
+		{1, 2, 3, 4},                          // false
+		{3, 1, 4, 2},                          // true
+		{3, 5, 0, 3, 4},                       // true
+		{-2, 1, 2, -2, 1, 2},                  // true
 	}
 	for _, tc := range testCases {
 		fmt.Println(find132pattern(tc))
 	}
-}
 
-type stack struct {
-	a []int
-}
-
-func (st *stack) size() int {
-	return len(st.a)
-}
-
-func (st *stack) isEmpty() bool {
-	return st.size() == 0
-}
-
-func (st *stack) peek() int {
-	return st.a[len(st.a)-1]
-}
-
-func (st *stack) second() int {
-	return st.a[len(st.a)-2]
-}
-
-func (st *stack) pop() int {
-	ret := st.peek()
-	st.a = st.a[:len(st.a)-1]
-	// fmt.Printf("popped: %d\n", ret)
-	return ret
-}
-
-func (st *stack) push(x int) {
-	st.a = append(st.a, x)
-	// fmt.Printf("pushed: %d\n", x)
 }
 
 func find132pattern(nums []int) bool {
-	stks := make([]stack, len(nums))
+	// fmt.Printf("unsorted = %v\n", unsort)
+	// fmt.Printf("nums = %v\n", nums)
 
-	for i, num := range nums {
-		stks[i].push(num)
-		fmt.Printf("pushed: %d into stack[%d], current = %+v\n", num, i, stks[i])
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			sorted := make([]int, len(nums))
+			copy(sorted, nums)
+			sort.Slice(sorted, func(i, j int) bool {
+				return sorted[i] < sorted[j]
+			})
 
-		for j := 0; j < len(stks); j++ {
-			switch {
-			case i == j:
-				continue
-			case stks[j].size() == 1:
-				if num > stks[j].peek() {
-					stks[j].push(num)
-					fmt.Printf("pushed: %d into stack[%d], current = %+v\n", num, j, stks[j])
-				}
-			case stks[j].size() == 2:
-				if num < stks[j].peek() && num > stks[j].second() {
-					return true
-				}
+			found := bSearchBetween(nums, sorted[i], sorted[j])
+			if found {
+				return true
 			}
 		}
 	}
 
 	return false
+}
+
+// search a num which grater than 'one' but less than 'three'
+func bSearchBetween(nums []int, one, three int) bool {
+	if len(nums) < 1 {
+		return false
+	}
+	left := 0
+	right := len(nums) - 1
+	midIndex := (left + right) / 2
+	target := nums[midIndex]
+
+	fmt.Printf("searching %d from nums: %v, checking if %d > %d and %d < %d?\n", target, nums, target, one, target, three)
+
+	if right < left {
+		return false
+	}
+	if right == left {
+		return target > one && target < three
+	}
+
+	// [1,2,3,4,5,6,7,8] 2,5,...
+
+	switch {
+	case target > one && target < three:
+		// fmt.Printf("found target = %d\n", target)
+		return true
+	case target > three:
+		return bSearchBetween(nums[:midIndex], one, three)
+	case target < one:
+		return bSearchBetween(nums[midIndex+1:], one, three)
+	}
+
+	// fmt.Printf("found target = %d\n", target)
+	return target > one && target < three
 }
